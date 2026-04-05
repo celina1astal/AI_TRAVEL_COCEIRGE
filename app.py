da5c5bf190f56f86c1a0cec2f02b5e1a419ca407
celinadas <celinadas111@gmail.com>
2026-04-05 17:59:26 +0530

    Update imports to use langchain_classic

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ from langchain_community.vectorstores import Chroma
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-# Modern 2026 Imports (Fixes the ModuleNotFoundError)
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain
+# FIXED 2026 IMPORTS (Note the _classic)
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains import create_retrieval_chain
+
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage, ToolMessage, SystemMessage, AIMessage
